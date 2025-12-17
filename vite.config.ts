4b72bb9677cb33f4b8c68e93b843b3f5e02c7dfc
Cristhian Velazquez <cristhian.velazquez@edge.com.py>
2025-12-16 21:56:37 -0300

    Fix base path in vite.config.ts to include trailing slash

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: "/diploma_cird",
+      base: "/diploma_cird/",
       server: {
         port: 3000,
         host: '0.0.0.0',
